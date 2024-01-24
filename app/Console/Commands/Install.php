e8ef2557f47ff03a285bec52be98dc1027199d02
Jesse Visser <hallo@jnvisser.nl>
2024-01-24 14:44:10 +0100

    Add command to install default bucket

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $bucket = 'public';
+        $path = bucket_path('', $bucket);
+
+        if (Storage::directoryExists($path)) {
+            $this->warn('Application already installed');
+
+            return;
+        }
+
+        $this->info('Installing application');
+
+        Storage::createDirectory($path);
+
+        $this->laravel->make('files')->link(
+            storage_path('app/bucket/'.$bucket),
+            public_path($bucket)
+        );
+
+        $this->info('Application installed');
+    }
+}
