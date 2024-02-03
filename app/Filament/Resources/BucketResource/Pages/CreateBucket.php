38d97144001ddae8127ff80b253a8b2e56a46d19
flowan <flowan@users.noreply.github.com>
2024-02-03 12:31:46 +0000

    Fix styling

--- app/Filament/Resources/BucketResource/Pages/CreateBucket.php
+++ app/Filament/Resources/BucketResource/Pages/CreateBucket.php
@@ -4,11 +4,9 @@ namespace App\Filament\Resources\BucketResource\Pages;
 
 use App\Filament\Resources\BucketResource;
 use App\Helpers\BucketHelper;
-use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
 
 class CreateBucket extends CreateRecord
 {
